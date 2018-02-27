32446ec77bc7f8fde5b2a25623ca7645aea2e58a
mohmmedwee <eloustah@gmail.com>
2018-02-27 11:20:44 +0200

    first commit

---
+++ system/storage/cache/84/848bb23eee9e5c01bcece7ecbfb7b5ed52c0a2cddf01e1d0b27076a86a480a01.php
@@ -0,0 +1,56 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_be6efe8a3ab2208c4d7c51f37d5c542066fb647c599308a3e25d334fa4919d6c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer>
+  <div class=\"container\"><a href=\"http://www.opencartarab.com\" target=\"_blank\">";
+        // line 2
+        echo (isset($context["text_project_arab"]) ? $context["text_project_arab"] : null);
+        echo "</a>|<a href=\"http://www.opencart.com\" target=\"_blank\">";
+        echo (isset($context["text_project"]) ? $context["text_project"] : null);
+        echo "</a>|<a href=\"http://www.opencart.com/index.php?route=documentation/introduction\" target=\"_blank\">";
+        echo (isset($context["text_documentation"]) ? $context["text_documentation"] : null);
+        echo "</a>|<a href=\"http://forum.opencart.com\" target=\"_blank\">";
+        echo (isset($context["text_support"]) ? $context["text_support"] : null);
+        echo "</a><br />
+    </div>
+</footer>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 2,  19 => 1,);
+    }
+}
+/* <footer>*/
+/*   <div class="container"><a href="http://www.opencartarab.com" target="_blank">{{text_project_arab}}</a>|<a href="http://www.opencart.com" target="_blank">{{ text_project }}</a>|<a href="http://www.opencart.com/index.php?route=documentation/introduction" target="_blank">{{ text_documentation }}</a>|<a href="http://forum.opencart.com" target="_blank">{{ text_support }}</a><br />*/
+/*     </div>*/
+/* </footer>*/
+/* </body></html>*/
+/* */
